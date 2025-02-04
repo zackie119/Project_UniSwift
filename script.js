bebb58b3f200caa010f39d04a1a693491fa58f81
zeyd <88453241+zackie119@users.noreply.github.com>
2025-02-05 00:11:47 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+document.querySelector(".download-btn").addEventListener("click", function () {
+    alert("We will notify you when the SwiftUni app is available!");
+});
+
+document.querySelector("form").addEventListener("submit", function (e) {
+    e.preventDefault();
+    alert("Thank you for reaching out! We will get back to you soon.");
+});
